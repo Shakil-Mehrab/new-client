4fc6247d2f052de7b4e1a07e961be75ac4a236a6
Shakil-Mehrab <mehrabhoussainshakil12@gmail.com>
2021-10-12 01:13:05 +0600

    commit with live absolute

--- pages/live/index.vue
+++ pages/live/index.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="relative -mt-4">
-    <div class="video-fixed">
-      <div class="mb-2 aspect-w-16 aspect-h-9">
+    <div :class="{ 'video-fixed': live_fixed }">
+      <div
+        class="mb-2 aspect-w-16 aspect-h-9"
+        :class="{ 'live-video-fixed live-video-protrait': live_fixed }"
+      >
         <iframe
-          class="live-video live-video-fixed"
+          class="live-video"
           name="ifram2"
           src="https://www.mcaster.tv/channel/dbcnews.php?u=dbcnews&amp;vw=100%&amp;vh=360"
           frameborder="0"
@@ -16,7 +19,7 @@
     <div class="container mx-auto mb-2 mt-7">
       <div class="w-full px-2">
         <Devider title="সর্বশেষ" />
-        <SidebarVideo :news="5" />
+        <SidebarVideo :news="20" />
       </div>
     </div>
   </div>
@@ -24,8 +27,31 @@
 <script>
 import SidebarVideo from "@/components/video/SidebarVideo";
 export default {
+  data() {
+    return {
+      live_fixed: false,
+    };
+  },
   components: {
     SidebarVideo,
+  },
+  methods: {
+    addScrollListener() {
+      let pxTrigger = 0;
+      const menuHeight = 140;
+      document.addEventListener("scroll", () => {
+        const pxFromTop = window.scrollY || window.pageYOffset;
+        if (pxFromTop > menuHeight) {
+          this.live_fixed = true;
+          console.log("hi");
+        } else {
+          this.live_fixed = false;
+        }
+      });
+    },
+  },
+  mounted() {
+    this.addScrollListener();
   },
 };
 </script>
